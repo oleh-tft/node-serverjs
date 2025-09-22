f860e6c2e4d6406c520a29c04e0e11ce6951a491
oleh-tft <tiftelalbion@gmail.com>
2025-09-22 22:36:00 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,135 @@
+import http from "http"
+import * as fs from "node:fs/promises"
+import mysql2 from 'mysql2'
+import GroupDao from './dao/groupDao.js'
+import { getAllowedMimeType } from './helper.js'
+
+const dbIniFilename = 'db.ini'
+const HTTP_PORT = 81;
+const CONTROLLERS_PATH = './controllers/'
+const CONTROLLER_FILE_SUFFIX = '_controller.js'
+
+const controllers = {}
+const loadControllersTask = fs.readdir(CONTROLLERS_PATH)
+    .then(async (filenames) => {
+        for (let filename of filenames) {
+            let pos = filename.indexOf(CONTROLLER_FILE_SUFFIX)
+            if (pos != -1) {
+                const controllerName = filename.substring(0, pos)
+                controllers[controllerName] = (await
+                    import(CONTROLLERS_PATH + filename)
+                ).default
+            }
+        }
+    })
+
+const dbIniFile = await fs.open(dbIniFilename, 'r')
+let dbConfig = {}
+
+for await (let line of dbIniFile.readLines()) {
+    let parts = line.split('#')
+    line = parts[0]
+    parts = line.split(';')
+    line = parts[0]
+    parts = line.split('=')
+    if (parts.length != 2) continue
+    dbConfig[parts[0].trim()] = parts[1].trim()
+}
+
+const dbPool = mysql2.createPool(dbConfig).promise()
+const groupDao = new GroupDao(dbPool)
+
+async function serverFunction(request, response) {
+    let parts = request.url.split("?")
+    if (parts.length > 2) {
+        response.writeHead(400)
+        response.end("Bad request")
+    }
+    let path = parts[0]
+    if (!path.endsWith('/')) {
+        let contentType = getAllowedMimeType(path)
+        if (contentType != null) {
+            const filePath = './wwwroot' + path
+            try {
+                await fs.access(filePath)
+                const stat = await fs.stat(filePath)
+                if (stat.isFile()) {
+                    (await fs.open(filePath, 'r')).createReadStream().pipe(response)
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    })
+                    return
+                }
+            } catch (_) { }
+        }
+    }
+
+    let components = path.split("/", 4);
+    let controller, action, id;
+    if (components[1].length > 0) {
+        controller = components[1].toLowerCase();
+    } else {
+        controller = "home";
+    }
+    if (components.length > 2 && components[2].length > 0) {
+        action = components[2].toLowerCase();
+    } else {
+        action = "index";
+    }
+    if (components.length > 3) {
+        id = components[3];
+    } else {
+        id = null;
+    }
+    console.log(controller, action, id);
+
+    if (typeof controllers[controller] == 'function') {
+        const controllerObject = new controllers[controller]
+        controllerObject.dbPool = dbPool
+        if (typeof controllerObject[action] == 'function') {
+            controllerObject[action](request, response, id)
+            return
+        } else {
+            response.writeHead(406)
+            response.end()
+            return
+        } 
+    } else {
+        response.writeHead(404)
+        response.end()
+        return
+    }
+}
+
+const server = http.createServer(serverFunction)
+server.on('close', () => {
+    console.log('Server stopped')
+    dbPool.end()
+    process.exit()
+})
+
+process.on('SIGINT', () => {
+    server.close()
+})
+
+await loadControllersTask
+console.log(controllers)
+
+server.listen(HTTP_PORT, () => {
+    console.log('Server listening port', HTTP_PORT)
+    console.log(`http://localhost:${HTTP_PORT}`)
+    console.log('Press Ctrl-C to stop')
+})
+
+
+
+// header line and body separator: /r/n
+// HTTP Request:
+// 9 Standart Methods:
+// GET - get,
+// POST - create,
+// PUT - replace, PATCH - update,
+// DELETE,
+// HEAD, TRACE, CONNECT,
+// OPTIONS
+// no body allowed for GET and HEAD
