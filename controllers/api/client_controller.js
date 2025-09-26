13e02bf9dad2ff3422a1b031d40803f6d3ed5679
oleh-tft <tiftelalbion@gmail.com>
2025-09-26 16:59:17 +0300

    Add files via upload

--- controllers/api/client_controller.js
+++ controllers/api/client_controller.js
@@ -6,15 +6,39 @@ export default class ClientController {
     }
 
     doPost(request, response, id) {
-        response.writeHead(200, {
-            'Content-Type': 'application/json'
+        //Прийом тіла запиту
+        let body = ''
+        request.on('data', function(chunk) { body += chunk })
+        request.on('end', function() {
+            console.log('ClientController::doPost body: ' + body)
+            console.log(request.headers['content-type'])
+            if (validContentType(request.headers['content-type'])) {
+                    let data = JSON.parse(body)
+                    if (typeof data == 'object')
+                        data = JSON.stringify(data)
+                    response.writeHead(200, {
+                        'Content-Type': 'application/json'
+                    })
+                    response.end(JSON.stringify({
+                        "controller": "ClientController",
+                        "method": "POST",
+                        "semantic": "Create",
+                        "body": data
+                    }))
+            } else {
+                response.writeHead(415) // Unsupported Media Type
+                response.end()
+            }
         })
-        response.end(JSON.stringify(
-            {
-                "controller": "ClientController",
-                "method": "POST",
-                "semantic": "Create"
-            }))
+        //response.writeHead(200, {
+        //    'Content-Type': 'application/json'
+        //})
+        //response.end(JSON.stringify(
+        //    {
+        //        "controller": "ClientController",
+        //        "method": "POST",
+        //        "semantic": "Create"
+        //    }))
     }
 
     doPut(request, response, id) {
@@ -46,5 +70,17 @@ export default class ClientController {
                 "semantic": "Delete"
             }))
     }
+}
 
+function validContentType(str) {
+    if (typeof str != 'string') return false
+
+    const jsonType = 'application/json'
+    if (str.startsWith(jsonType)) {
+        if (str === jsonType || str[jsonType.length] == ';') {
+            return true
+        }
+    }
+
+    return false
 }
